b8fd7c971f0bc6ea02d5ced54b2ba74e6552e5ab
Nikita <mykytamaliarenko@gmail.com>
2019-11-01 11:23:28 +0200

    added package parse serial number  added get number

---
+++ ParseSerialNumber/ParseNumber.py
@@ -0,0 +1,10 @@
+def get_number(serial_number: str) -> int:
+    """
+    :param serial_number: str, input from user, record book serial number, KB №12649664
+    :return: int, number, 12649664
+    """
+    return 0
+
+
+if __name__ == "__main__":
+    get_number("KB №12649664")
