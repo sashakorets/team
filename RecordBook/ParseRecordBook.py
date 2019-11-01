6a44fe8b2a2a882a47826a66731cc8cdcd1b5bda
igortereshchenko <igortereshchenko@gmail.com>
2019-11-01 11:01:04 +0200

    add main.py

--- RecordBook/ParseRecordBook.py
+++ RecordBook/ParseRecordBook.py
@@ -5,4 +5,10 @@ def parse_record_book(text=''):
     :param text: тип даних, звідки беруться, який формат, приклади
     :return: тип даних, який формат, приклади
     """
+    print(text)
     return None
+
+
+if __name__=='__main__':
+
+    parse_record_book('some data')
---
+++ main.py
@@ -0,0 +1,4 @@
+
+from RecordBook.ParseRecordBook import parse_record_book
+
+parse_record_book('new data')
