24d5c2a3a6f73264be64fe0875d40badfeb7cb74
Nikita <mykytamaliarenko@gmail.com>
2019-11-01 11:18:25 +0200

    added package parse month  added get month

---
+++ ParseMonth/ParseMonth.py
@@ -0,0 +1,11 @@
+def get_month(date: str) -> str:
+    """
+
+    :param date: str, input from user, date, 1 вересня 2019 р.
+    :return: str input from user, month, вересень
+    """
+    return ""
+
+
+if __name__ == "__main__":
+    get_month("1 вересня 2019 р.")
